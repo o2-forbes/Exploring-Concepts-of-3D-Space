0f3d7e28091bf55b7bf21bbae301c5b5098cd809
Ollie <oliver2.forbes@live.uwe.ac.uk>
2024-03-19 14:33:09 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,23 @@
+let negEdge = -100;
+let posEdge = 100;
+let count = 0;
+
 function setup() {
-  createCanvas(400, 400);
+	createCanvas(500,500, WEBGL);
 }
 
 function draw() {
-  background(220);
+	background(0);
+	ambientLight(255,255,255);
+
+	rotateY(count);
+	count = count + (0.003);
+
+	stroke(255);
+	drawEdges();
+}
+
+function drawEdges() {
+	// draw lines here 
+
 }
